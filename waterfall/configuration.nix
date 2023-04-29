39a1cb0f687bebfd784fba5ec34ff9e210cc59be
Tim Sutton <tim@kartoza.com>
2023-04-29 12:27:39 +0100

    Refactored locale

--- waterfall/configuration.nix
+++ waterfall/configuration.nix
@@ -11,6 +11,7 @@
       ../vim.nix
       ../tim.nix
       ../nvidia.nix
+      ../locale.nix
     ];
 
   # Bootloader.
@@ -32,24 +33,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Lisbon";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_PT.UTF-8";
-    LC_IDENTIFICATION = "pt_PT.UTF-8";
-    LC_MEASUREMENT = "pt_PT.UTF-8";
-    LC_MONETARY = "pt_PT.UTF-8";
-    LC_NAME = "pt_PT.UTF-8";
-    LC_NUMERIC = "pt_PT.UTF-8";
-    LC_PAPER = "pt_PT.UTF-8";
-    LC_TELEPHONE = "pt_PT.UTF-8";
-    LC_TIME = "pt_PT.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
