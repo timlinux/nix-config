884efb6bbb92cb8f5756f7a861da510f729d85ef
Tim Sutton <tim@kartoza.com>
2023-05-01 00:18:46 +0100

    Fix vim backspace

--- config/vim.nix
+++ config/vim.nix
@@ -37,6 +37,8 @@
         " Start NERDTree when Vim is started without file arguments.
         autocmd StdinReadPre * let s:std_in=1
         autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
+        " Make backspace work
+        set backspace=indent,eol,start 
       '';
     })
   ];
--- hosts/waterfall/configuration.nix
+++ hosts/waterfall/configuration.nix
@@ -16,6 +16,7 @@
       ../../config/locale.nix
       ../../config/yubikey.nix
       ../../config/ssh.nix
+      ../../config/cron-waterfall.nix
     ];
 
   # Bootloader.
