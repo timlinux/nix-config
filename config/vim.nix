96eed181539650566d658812d52da22a947a7258
Tim Sutton <tim@kartoza.com>
2023-05-03 09:00:03 +0100

    Adding vim shortcuts

--- config/vim.nix
+++ config/vim.nix
@@ -39,6 +39,43 @@
         autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
         " Make backspace work
         set backspace=indent,eol,start 
+
+	" Next group of entries taken from the excellent article at:
+	" https://realpython.com/vim-and-python-a-match-made-in-heaven/
+        " split navigations
+        " Remaps moving around splits to match the movement keys
+        " 
+        "    Ctrl+J move to the split below
+    	"    Ctrl+K move to the split above
+	"    Ctrl+L move to the split to the right
+    	"    Ctrl+H move to the split to the left
+        " 
+	nnoremap <C-J> <C-W><C-J>
+	nnoremap <C-K> <C-W><C-K>
+	nnoremap <C-L> <C-W><C-L>
+	nnoremap <C-H> <C-W><C-H>
+
+        " PEP8 Indentation
+	au BufNewFile,BufRead *.py
+	    \ set tabstop=4
+	    \ set softtabstop=4
+	    \ set shiftwidth=4
+	    \ set textwidth=79
+	    \ set expandtab
+	    \ set autoindent
+	    \ set fileformat=unix
+
+	" Nice indents for html, js, css etc
+	au BufNewFile,BufRead *.js, *.html, *.css
+	    \ set tabstop=2
+	    \ set softtabstop=2
+	    \ set shiftwidth=2
+
+	set encoding=utf-8
+    
+	" Cut and paste from the system keyboard
+	set clipboard=unnamed
+
       '';
     })
   ];
