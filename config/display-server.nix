066c5656b80ea7821085ab116fc8c6c3235a37ec
Tim Sutton <tim@kartoza.com>
2023-06-08 21:48:49 +0100

    Disable wayland

--- config/display-server.nix
+++ config/display-server.nix
@@ -3,15 +3,15 @@
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  
-  hardware.opengl.enable = true;
+  #hardware.opengl.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Set gnome to run on x11 or wayland
-  services.xserver.displayManager.gdm.wayland = true; 
-  programs.xwayland.enable = true;
+  # Currently I have issues running wayland like flashing windows etc.
+  services.xserver.displayManager.gdm.wayland = false; 
+  programs.xwayland.enable = false;
 
 }
