b77ac39cab1950885bf9e26f295d656dbcbf5098
Tim Sutton <tim@kartoza.com>
2023-06-13 12:08:00 +0100

    Comment out wayland stuff

--- config/display-server.nix
+++ config/display-server.nix
@@ -4,12 +4,5 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   #hardware.opengl.enable = true;
-  programs.xwayland.enable = true;
-
-  # Add system wide packages
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-    xdg-desktop-portal   
-  ];
 
 }
