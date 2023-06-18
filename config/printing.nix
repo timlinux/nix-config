85ca9d2ba0ce3e309eabdd478f512b831fe5b36a
Tim Sutton <tim@kartoza.com>
2023-06-18 23:29:10 +0100

    Printing tweaks

--- config/printing.nix
+++ config/printing.nix
@@ -4,4 +4,9 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.startWhenNeeded = true;
+  services.printing.drivers = with pkgs; [
+    gutenprint
+    hplip
+  ];
 }
