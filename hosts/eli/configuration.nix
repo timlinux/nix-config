74bc78861a0e71947feb952733574f5ce153dfca
Tim Sutton <tim@kartoza.com>
2023-06-20 08:05:35 +0100

    Eli configs

--- hosts/eli/configuration.nix
+++ hosts/eli/configuration.nix
@@ -5,55 +5,9 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #../../config/android-sdk.nix
-      #../../config/upgrades.nix
-      #../../config/arduino.nix
-      #../../config/conda.nix
-      #../../config/biometrics.nix
-      #../../config/ccache.nix
-      #../../config/console-apps.nix
-      #../../config/cron-waterfall.nix
-      #../../config/dir-env.nix
-      #../../config/docker.nix
-      #../../config/fonts.nix
-      #../../config/games.nix
-      #../../config/display-server.nix
-      #../../config/hyperland-desktop.nix
-      #../../config/gnome-desktop-x11.nix
-      #../../config/gnome-desktop-wayland.nix
-      #../../config/deepin-desktop.nix
-      #../../config/budgie-deskop.nix
-      #../../config/plasma-desktop.nix
-      #../../config/xfce-desktop.nix
-      #../../config/pantheon-desktop.nix
-      #../../config/gnome-desktop-apps.nix
-      #../../config/gui-apps.nix
-      #../../config/iphone.nix
-      #../../config/locale-za-en.nix
-      # Dont enable when using wayland - causes screen flickr
-      #../../config/nvidia.nix
-      #../../config/ntfs.nix
-      #../../config/obs.nix
-      #../../config/postgres.nix
-      #../../config/printing.nix
-      #../../config/python.nix
-      #../../config/qgis.nix
-      #../../config/qgis-dev.nix
-      #../../config/ssh.nix
-      #../../config/starship.nix
-      #../../config/syncthing.nix
-      #../../config/tailscale.nix
-      #../../config/uxplay.nix
-      #../../config/vim.nix
-      #../../config/yubikey.nix
-      #../../config/vscode.nix
-      #../../config/unstable-apps.nix
-      #../../users/all.nix
-      #../../users/eli.nix
     ];
 
   # Bootloader.
@@ -64,11 +18,12 @@
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
   };
+
   # Enable swap on luks
   boot.initrd.luks.devices."luks-4a1f442c-e191-4f92-8b47-b580db36e463".device = "/dev/disk/by-uuid/4a1f442c-e191-4f92-8b47-b580db36e463";
   boot.initrd.luks.devices."luks-4a1f442c-e191-4f92-8b47-b580db36e463".keyFile = "/crypto_keyfile.bin";
 
-  networking.hostName = "eli"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -96,14 +51,9 @@
     layout = "za";
     xkbVariant = "";
   };
-  ## Tweak by Tim to use adguard home
-  #networking.nameservers = [ "100.100.68.130" ];
-  # These last two are the public adguard services to block
-  # ads, adult content etc. They will be overwritten by
-  # tailscale if it is running, so they are just a backup for
-  # when tailscale is down...
-  # See https://adguard-dns.io/kb/general/dns-providers/?utm_campaign=dns_kb_providers&utm_medium=ui&utm_source=home
-  #networking.nameservers = [ "94.140.14.15" "94.140.15.16" ];
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -122,15 +72,42 @@
     #media-session.enable = true;
   };
 
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eli = {
+    isNormalUser = true;
+    description = "eli";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -145,20 +122,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
-  ### 
-  ### Additions by Tim (see also pkgs sections above)
-  ###
-  
-  ### Flakes support
-      
-  ### See https://nixos.wiki/wiki/Flakes
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  ###
-  ### Flatpack support
-  ### see https://flatpak.org/setup/NixOS
-  services.flatpak.enable = true;
-
 
 }
