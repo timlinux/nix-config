fb08f657cffe3d23bf1761d0f6bf0f8cd7a95403
Tim Sutton <tim@kartoza.com>
2023-07-16 00:02:13 +0100

    Notes for bootsplash

--- config/bootsplash.nix
+++ config/bootsplash.nix
@@ -11,6 +11,8 @@
   boot.plymouth = {
      enable = true;
      themePackages = [ pkgs.adi1090x-plymouth ];
+     # See https://github.com/adi1090x/plymouth-themes
+     # for a list of other awesome themes
      theme = "lone"; # provided in ../packages
      # Or comment out the above two lines and use this:
      # boot.plymouth.theme = "breeze"; # default is bgrt which shows manufacturer logo
