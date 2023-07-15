7883fe12aa2c84d41839a3fff268145bffa475b3
Tim Sutton <tim@kartoza.com>
2023-07-15 23:40:17 +0100

    Adding my first overlay

--- config/bootsplash.nix
+++ config/bootsplash.nix
@@ -2,10 +2,18 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [ (import ../packages) ];
+
   # Boot splash stuff
   boot.initrd.systemd.enable = true;
-  boot.plymouth.enable = true;
   boot.kernelParams = [ "quiet" ]; # suppresses console text before the pwd prompt
   #boot.plymouth.logo = "/boot/kartoza-wallpaper.png";
-  boot.plymouth.theme = "breeze"; # default is bgrt which shows manufacturer logo
+  boot.plymouth = {
+     enable = true;
+     themePackages = [ pkgs.adi1090x-plymouth ];
+     theme = "lone"; # provided in ../packages
+     # Or comment out the above two lines and use this:
+     # boot.plymouth.theme = "breeze"; # default is bgrt which shows manufacturer logo
+  };
+ 
 }
