8216f7df0803fa26b5d2e19f0899f20a1efaa398
Tim Sutton <tim@kartoza.com>
2023-08-02 22:15:01 +0100

    Adding configs for amy and luna

--- hosts/amz/mklinks.sh
+++ hosts/amz/mklinks.sh
@@ -1,6 +1,7 @@
 #!/run/current-system/sw/bin/bash
 cd /etc/nixos
 cp /etc/nixos/hardware-configuration.nix /root/nix-config/hosts/amy/hardware-configuration.nix
+cp /etc/nixos/configuration.nix /root/nix-config/hosts/amy/configuration.nix
 sudo mv configuration.nix configuration.nix.original
 sudo mv hardware-configuration.nix hardware-configuration.nix.original
 sudo ln -s /root/nix-config/hosts/amz/configuration.nix /etc/nixos/configuration.nix
