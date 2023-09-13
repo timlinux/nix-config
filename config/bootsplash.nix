5bae3f5ea18c2e1d983b21c69f4db05337e50e3e
Tim Sutton <tim@kartoza.com>
2023-09-13 22:20:25 +0100

    Adding kartoza theme

--- config/bootsplash.nix
+++ config/bootsplash.nix
@@ -10,10 +10,10 @@
   #boot.plymouth.logo = "/boot/kartoza-wallpaper.png";
   boot.plymouth = {
      enable = true;
-     themePackages = [ pkgs.adi1090x-plymouth ];
+     themePackages = [ pkgs.kartoza-plymouth ];
      # See https://github.com/adi1090x/plymouth-themes
      # for a list of other awesome themes
-     theme = "lone"; # provided in ../packages
+     theme = "flame"; # provided in ../packages
      # Or comment out the above two lines and use this:
      # boot.plymouth.theme = "breeze"; # default is bgrt which shows manufacturer logo
   };
