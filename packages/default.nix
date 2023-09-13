e96f491e55981e077b10621dbf944127ecafe586
Tim Sutton <tim@kartoza.com>
2023-09-13 22:18:41 +0100

    Adding kartoza theme

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,6 @@
 self: super:
 {
-  adi1090x-plymouth = super.callPackage ./adi1090x-plymouth { };
+  kartoza-plymouth = super.callPackage ./kartoza-plymouth { };
   # This will build the LTR version
   qgis-ltr = super.callPackage ./qgis/ltr.nix { };
   # This will build the latest version but with QScintilla api disabled (see https://github.com/NixOS/nixpkgs/issues/248239)
---
+++ packages/kartoza-plymouth/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "kartoza-plymouth";
+  version = "0.0.1";
+
+  src = builtins.fetchGit {
+    url = "https://github.com/timlinux/kartoza-plymouth-themes.git";
+  };
+
+  buildInputs = [
+    pkgs.git
+  ];
+
+  configurePhase = ''
+mkdir -p $out/share/plymouth/themes/
+  '';
+
+  buildPhase = ''
+  '';
+
+  installPhase = ''
+  cp -r pack_1/flame $out/share/plymouth/themes
+cat pack_1/flame/flame.plymouth | sed  "s@\/usr\/@$out\/@" > $out/share/plymouth/themes/flame/flame.plymouth
+  cp -r pack_1/kartoza $out/share/plymouth/themes
+cat pack_1/kartoza/kartoza.plymouth | sed  "s@\/usr\/@$out\/@" > $out/share/kartoza/themes/kartoza/kartoza.plymouth
+  '';
+}
+
