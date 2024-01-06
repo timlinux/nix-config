678f09f0c4c3ca477e320ba2a1c4570c35ce040c
Tim Sutton <tim@kartoza.com>
2024-01-06 17:29:45 +0000

    Trying to make screen-control work

--- config/screen-control.nix
+++ config/screen-control.nix
@@ -11,9 +11,18 @@
   # See https://discourse.nixos.org/t/how-to-enable-ddc-brightness-control-i2c-permissions/20800
   # And https://github.com/ddccontrol/ddccontrol
 
+  # I think this makes the lines below redundant
+  hardware.i2c.enable = true;
+
+
   boot.kernelModules = ["i2c-dev"];
   services.udev.extraRules = ''
         KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
   '';
+  # Add system wide packages
+  environment.systemPackages = with pkgs; [
+     ddccontrol 
+     ddcutil
+  ];
 
 }
