33350365680e425046897e51d25e10a55682c75c
Tim Sutton <tim@kartoza.com>
2024-03-21 00:43:40 +0000

    Use hw from test machine for eoi

--- eoi/hardware-configuration.nix
+++ eoi/hardware-configuration.nix
@@ -1,12 +1,7 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
-  };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/3798-8FE0"; fsType = "vfat"; };
+  boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
