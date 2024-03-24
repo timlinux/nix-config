cd1966f834efce67c5cf830fa98e17b5054fb9da
Tim Sutton <tim@kartoza.com>
2024-03-24 23:57:03 +0000

    Initial conversion to flake

--- flake.nix
+++ flake.nix
@@ -1,49 +1,31 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    description = "Kartoza NixOS Flakes";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+      home-manager.url = "github:nix-community/home-manager/release-23.11";
     };
-  };
 
-  outputs = inputs: {
-    nixosConfigurations = {
-      myhost = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          inputs.home-manager.nixosModules.home-manager
-        ];
-        config = {
-          services.xserver.enable = true;
-          services.xserver.displayManager.gdm.enable = true;
-          services.xserver.desktopManager.gnome.enable = true;
-          environment.gnome.excludePackages = (with pkgs; [
-            gnome-photos
-            gnome-tour
-          ]) ++ (with pkgs.gnome; [
-            cheese # webcam tool
-            gnome-music
-            gedit # text editor
-            epiphany # web browser
-            geary # email reader
-            gnome-characters
-            tali # poker game
-            iagno # go game
-            hitori # sudoku game
-            atomix # puzzle game
-            yelp # Help view
-            gnome-contacts
-            gnome-initial-setup
-          ]);
-          programs.dconf.enable = true;
-          environment.systemPackages = with pkgs; [
-            gnome.gnome-tweaks
-          ]
+    outputs = { self, home-manager, nixpkgs }@inputs:
+    let
+      system = "x86_64-linux";
+      specialArgs = inputs // { inherit system; };
+      shared-modules = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+          };
+        }
+      ];
+    in {
+        nixosConfigurations = {
+          crest = nixpkgs.lib.nixosSystem {
+            specialArgs = specialArgs;
+	    system = system;
+            modules = shared-modules ++ [ ./crest.nix ];
+          };
         };
       };
-    };
-  };
 }
