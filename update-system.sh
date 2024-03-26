abc8c738e8aa4db08c1f5db7a77791f6b6d874d5
Tim Sutton <tim@kartoza.com>
2024-03-26 10:34:53 +0000

    Reenbale all modules for flakes

--- update-system.sh
+++ update-system.sh
@@ -1,9 +1,13 @@
-#!/run/current-system/sw/bin/bash
+#!/usr/bin/env bash
 
 # Fetch latest changes and deploy
 # Expects this repo to be checked out in /root/nix-config
 # Tim Sutton 2023
 
-cd /root/nix-config
-nix-shell -p git --run "git pull"
-nixos-rebuild switch
+if [ "$EUID" -ne 0 ]
+  then 
+    echo "🛑Run this as SUDO!"
+  exit
+fi
+
+nixos-rebuild switch --flake .
