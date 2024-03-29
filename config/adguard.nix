d5f6ebe8358716227a04b97a592cd11f34cca6cb
Tim Sutton <tim@kartoza.com>
2024-03-29 08:42:33 +0000

    Adding adguard

--- config/adguard.nix
+++ config/adguard.nix
@@ -1,25 +1,31 @@
+# Note I needed to restart my computer after setting this up
 { config, pkgs, ... }:
-{
-  let adguardPort = 3000;
-  in {
-    config = {
-      networking = {
-        firewall = {
-          allowedTCPPorts = [ adguardPort ];
-          allowedUDPPorts = [ 53 ];
-        };
+let adguardPort = 3000;
+in {
+  config = {
+    networking = {
+      nameservers = [ "127.0.0.1" "::1" ];
+      # If using dhcpcd:
+      dhcpcd.extraConfig = "nohook resolv.conf";
+      # If using NetworkManager:
+      networkmanager.dns = "none";
+      firewall = {
+        allowedTCPPorts = [ adguardPort ];
+        allowedUDPPorts = [ 53 ];
       };
-  
-      services = {
-        adguardhome = {
-          enable = true;
-          openFirewall = true;
-          settings = {
-            bind_port = adguardPort;
-            schema_version = 20;
-          };
+    };
+
+    services = {
+      adguardhome = {
+        enable = true;
+        mutableSettings = true;
+        allowDHCP = false;
+        openFirewall = false;
+        settings = {
+          bind_port = adguardPort;
+          #schema_version = 20;
         };
       };
     };
-  }
+  };
 }
