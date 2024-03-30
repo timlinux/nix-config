64a075094be3e4ed7ba0a488dc839bb385d60231
Tim Sutton <tim@kartoza.com>
2024-03-30 21:12:39 +0000

    Grub theme kartoza branding

--- config/grub-theme.nix
+++ config/grub-theme.nix
@@ -2,12 +2,12 @@
 {
   boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
     pname = "distro-grub-themes";
-    version = "3.1";
+    version = "1";
     src = pkgs.fetchFromGitHub {
-      owner = "AdisonCavani";
-      repo = "distro-grub-themes";
-      rev = "v3.1";
-      hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      owner = "kartoza";
+      repo = "kartoza-grub-theme";
+      rev = "main";
+      hash = "";
     };
     installPhase = "cp -r customize/nixos $out";
   };
