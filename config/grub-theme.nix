48eb88f0c8ba214d3962884d97050cbe3d672356
Tim Sutton <tim@kartoza.com>
2024-04-01 08:23:00 +0100

    Use latest grub theme tweak

--- config/grub-theme.nix
+++ config/grub-theme.nix
@@ -8,7 +8,7 @@
       owner = "kartoza";
       repo = "kartoza-grub-theme";
       rev = "main";
-      hash = "sha256-wQCyO7Tqtz28ewDH21rVbE0lHRvGpDH9bq/Iop3vwxc=";
+      hash = "sha256-JETB/tU1NkbEXVUzqRD3q92VewT2HjaTgdCHuenQonw=";
     };
     installPhase = "ls -lah themes; mkdir nixos; tar -xf themes/nixos.tar -C nixos; ls -lah; cp -r nixos $out/";
   };
