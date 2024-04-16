a008a64a82690edfe797e5bc4b22712004e0e18f
Tim Sutton <tim@kartoza.com>
2024-04-16 01:01:23 +0100

    starship (fixed)

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -2,7 +2,9 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    # This does not work so at the moment we have
+    # the global kitty config in modules/kitty.nix
     # Configuration written to ~/.config/starship.toml
-    settings = { builtins.readFile ../../dotfiles/starship.toml };
+    #settings = { builtins.readFile ../../dotfiles/starship.toml };
   };
 }
