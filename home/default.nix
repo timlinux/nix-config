e746b7de9a5c43a012fd572ab231fa4caaceec72
Tim Sutton <tim@kartoza.com>
2024-04-18 21:27:19 +0100

    Starship config

--- home/default.nix
+++ home/default.nix
@@ -2,19 +2,18 @@
   imports = [
     ./gtk/default.nix
     ./kitty/default.nix
-    # Broken, use the module approach rather
-    #./starship/default.nix
+    ./starship/default.nix
     ./direnv/default.nix
     ./xdg/default.nix
     ./git/default.nix
   ];
   home.file."Kartoza.txt".text = "This computer is property of Kartoza.com";
-  programs.starship = {
-    enable = true;
-    settings = ''
-      ${builtins.readFile ../dotfiles/starship.toml.nix}
-    '';
-  };
+  #programs.starship = {
+  #  enable = true;
+  #  settings = ''
+  #    ${builtins.readFile ../dotfiles/starship.toml.nix}
+  #  '';
+  #};
   #home.file."starship.toml".text = builtins.readFile ../dotfiles/starship.toml;
   
 
