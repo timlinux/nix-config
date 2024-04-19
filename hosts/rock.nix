92a17e4df59b7d113f6cca8c0c782f6d1393dcec
Tim Sutton <tim@kartoza.com>
2024-04-19 23:04:33 +0100

    Added display server to configs for gnome and vm

--- hosts/rock.nix
+++ hosts/rock.nix
@@ -1,47 +1,51 @@
-{ config, lib, pkgs, modulesPath, ... }:
 {
-  imports =
-    [ ()
-      ../configuration/virtual-machine.nix
-      ../modules/locale-pt-en.nix
-      ../modules/zfs-encryption.nix
-      ../users/guest.nix
-      ../users/timlinux.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    ../configuration/virtual-machine.nix
+    ../modules/locale-pt-en.nix
+    ../modules/zfs-encryption.nix
+    ../users/guest.nix
+    ../users/timlinux.nix
+  ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "NIXROOT/root";
-      fsType = "zfs";
-    };
+  fileSystems."/" = {
+    device = "NIXROOT/root";
+    fsType = "zfs";
+  };
 
-  fileSystems."/nix" =
-    { device = "NIXROOT/nix";
-      fsType = "zfs";
-    };
+  fileSystems."/nix" = {
+    device = "NIXROOT/nix";
+    fsType = "zfs";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/vda1";
-      #use uuid rather
-      #device = "/dev/disk/by-uuid/D7EC-2233";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/vda1";
+    #use uuid rather
+    #device = "/dev/disk/by-uuid/D7EC-2233";
+    fsType = "vfat";
+  };
 
-  fileSystems."/home" =
-    { device = "NIXROOT/home";
-      fsType = "zfs";
-    };
+  fileSystems."/home" = {
+    device = "NIXROOT/home";
+    fsType = "zfs";
+  };
 
   networking.hostName = "rock"; # Define your hostname.
-  # See https://search.nixos.org/options?channel=unstable&show=networking.hostId&query=networking.hostId 
+  # See https://search.nixos.org/options?channel=unstable&show=networking.hostId&query=networking.hostId
   # Generate using this:
   # head -c 8 /etc/machine-id
   networking.hostId = "0c83c5df"; # needed for zfs
-  swapDevices = [ ];
+  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
