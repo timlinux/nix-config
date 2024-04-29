6c3dd8a4af5f0897d1081de1f80365f5bafc554d
Tim Sutton <tim@kartoza.com>
2024-04-29 17:04:26 +0100

    Rolled back some hardening

--- modules/harden.nix
+++ modules/harden.nix
@@ -11,9 +11,9 @@
   lib,
   ...
 }: {
-  imports = [
-    <nixpkgs/nixos/modules/profiles/hardened.nix>
-  ];
+ # imports = [
+ #   <nixpkgs/nixos/modules/profiles/hardened.nix>
+ # ];
 
   config = {
     # Ignore ICMP broadcasts to avoid participating in Smurf attacks
@@ -64,33 +64,33 @@
     #
 
     # enable firewall and block all ports
-    networking.firewall.enable = true;
-    networking.firewall.allowedTCPPorts = [];
-    networking.firewall.allowedUDPPorts = [];
+    #networking.firewall.enable = true;
+    #networking.firewall.allowedTCPPorts = [];
+    #networking.firewall.allowedUDPPorts = [];
 
     # disable coredump that could be exploited later
     # and also slow down the system when something crash
     systemd.coredump.enable = false;
 
     # required to run chromium
-    security.chromiumSuidSandbox.enable = true;
+    #security.chromiumSuidSandbox.enable = true;
 
     # enable firejail
-    programs.firejail.enable = true;
+    #programs.firejail.enable = true;
 
     # create system-wide executables firefox and chromium
     # that will wrap the real binaries so everything
     # work out of the box.
-    programs.firejail.wrappedBinaries = {
-      firefox = {
-        executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
-        profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-      };
-      chromium = {
-        executable = "${pkgs.lib.getBin pkgs.chromium}/bin/chromium";
-        profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
-      };
-    };
+    #programs.firejail.wrappedBinaries = {
+    #  firefox = {
+    #    executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
+    #    profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+    #  };
+    #  chromium = {
+    #    executable = "${pkgs.lib.getBin pkgs.chromium}/bin/chromium";
+    #    profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
+    #  };
+    #};
 
     # enable antivirus clamav and
     # keep the signatures' database updated
