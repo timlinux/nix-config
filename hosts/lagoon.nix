6d93f70855a35ea2d01af356773225e15af90e4d
Tim Sutton <tim@kartoza.com>
2024-05-16 22:10:55 +0100

    Vicky machine added

---
+++ hosts/lagoon.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  # Vicky Laptop
+
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ../configuration/desktop-gnome-x11.nix
+    ../configuration/desktop-apps.nix
+    ../software/system/locale-pt-en.nix
+    ../software/system/biometrics.nix
+    ../software/system/zfs-encryption.nix
+    ../modules/unstable-apps.nix # qgis, keepasxc, vscode, uxplay
+    ../software/system/podman.nix
+    ../software/system/distrobox.nix
+    ../software/system/virt.nix
+    ../software/system/sanoid.nix
+    ../users/vicky.nix
+    ../users/tim.nix
+  ];
+
+  boot.initrd.availableKernelModules = ["nvme" "ehci_pci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "NIXROOT/root";
+    fsType = "zfs";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/EB5C-D827";
+    fsType = "vfat";
+  };
+
+  fileSystems."/home" = {
+    device = "NIXROOT/home";
+    fsType = "zfs";
+  };
+
+  networking.hostName = "lagoon"; # Define your hostname.
+  # See https://search.nixos.org/options?channel=unstable&show=networking.hostId&query=networking.hostId
+  # Generate using this:
+  # head -c 8 /etc/machine-id
+  networking.hostId = "d43e0d41"; # needed for zfs
+  swapDevices = [];
+
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
---
+++ packages/itk4/manual-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# For manualy building ITK 4.13.1
+# nix develop
+# git clone git@github.com:InsightSoftwareConsortium/ITK.git
+# cd ITK
+# mkdir build
+# cd build
+# You make need to adjust the path of libminc below
+#
+# ccmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=ON -DITK_FORBID_DOWNLOADS=ON -DITK_USE_SYSTEM_LIBRARIES=ON -DITK_USE_SYSTEM_EIGEN=OFF -DITK_USE_SYSTEM_GOOGLETEST=OFF -DITK_USE_SYSTEM_GDCM=ON -DITK_USE_SYSTEM_MINC=ON -DLIBMINC_DIR=/nix/store/chdd53qpv2vrp9nfm8x91dl1v1nf036c-libminc-2.4.05/lib/cmake -DModule_ITKMINC=ON -DModule_ITKIOMINC=ON -DModule_ITKIOTransformMINC=ON -DModule_SimpleITKFilters=OFF -DModule_ITKVtkGlue=ON -DModule_ITKReview=ON -DModule_MGHIO=OFF -DModule_AdaptiveDenoising=OFF -DModule_GenericLabelInterpolator=OFF
