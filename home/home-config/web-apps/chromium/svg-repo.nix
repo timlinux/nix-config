fc2bde6f0a35bce5e9530998d60bbd5163f0be20
Tim Sutton <tim@kartoza.com>
2024-05-18 17:16:20 +0100

    SVG Repo Tweak

--- home/home-config/web-apps/chromium/svg-repo.nix
+++ home/home-config/web-apps/chromium/svg-repo.nix
@@ -2,24 +2,32 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  siteUrl = "https://www.svgrepo.com/";
+  appName = "SVG Repo";
+  iconName = "svg-repo.svg";
+  iconPath = "${config.home.homeDirectory}/.local/share/icons/${iconName}";
+in {
+  home.file."whatsapp_image" = {
+    source = ./${iconName};
+    target = iconPath;
+  };
   home.packages = with pkgs; let
-    svg-repo = makeDesktopItem {
-      name = "SVG Repo";
-      desktopName = "SVG Repo";
-      genericName = "SVG Repo";
+    svgRepo = makeDesktopItem {
+      name = appName;
+      desktopName = appName;
+      genericName = appName;
       exec = ''
-        ${config.programs.chromium.package}/bin/chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app="https://www.svgrepo.com/"'';
-      icon = "nix";
+        ${config.programs.chromium.package}/bin/chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app="${siteUrl}"'';
+      icon = iconPath;
       # See https://specifications.freedesktop.org/menu-spec/latest/apa.html
       categories = ["Network" "Graphics"];
-      mimeTypes = ["x-scheme-handler/teams"];
       # Needed for the gnome panel icon to be correct
       # and not to stack with other chrome apps
       # You can find out the value to use by running
       # xprop and then clicking on the app window
       # Then set the value below to whatever WM_CLASS(STRING) is
-      startupWMClass = "mail.google.com";
+      startupWMClass = "www.svgrepo.com";
     };
-  in [svg-repo];
+  in [svgRepo];
 }
