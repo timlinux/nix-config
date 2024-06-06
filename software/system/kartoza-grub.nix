10ffe6c27cf73a6099778956ba5e0b81493c2f7f
Tim Sutton <tim@kartoza.com>
2024-06-06 08:51:11 +0100

    splash  and grub updates

--- software/system/kartoza-grub.nix
+++ software/system/kartoza-grub.nix
@@ -5,4 +5,9 @@
 }: {
   nixpkgs.overlays = [(import ../../packages)];
   boot.loader.grub.theme = pkgs.kartoza-grub;
+  # This is for the intermediate screen
+  # between choosing a boot option and when
+  # plymouth starts
+  # See https://discourse.nixos.org/t/how-to-use-boot-loader-grub-splashimage/23748
+  boot.loader.grub.splashImage = ../../resources/kartoza-background.gdm.png;
 }
