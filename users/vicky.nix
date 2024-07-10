5852bac17977b162dd0a3a52fe8bff1336eb13a8
Victoria Neema <victoria@kartoza.com>
2024-07-11 00:22:50 +0300

    Custom git settings for user vicky

--- users/vicky.nix
+++ users/vicky.nix
@@ -77,6 +77,12 @@ in {
           extraConfig = {
             github.user = "vikineema";
             gitlab.user = "victoria@kartoza.com";
+            branch.autoSetupRebase = "never";
+            pull.ff = "true";
+            push = {
+              default = "current";
+              autoSetupRemote = true;
+            };
           };
           # rest of git is configured in ../home/git..
         };
